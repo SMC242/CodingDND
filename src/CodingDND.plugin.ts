a96a0a8e20a36c262e0bd375c5a858caf5f7771d
SMC242 <mcalonansam2@gmail.com>
2020-10-05 00:15:42 +0100

    Finished metadata and comverted to TypeScript

--- src/CodingDND.plugin.ts
+++ src/CodingDND.plugin.ts
@@ -1,16 +1,14 @@
 /**
- * @name ExamplePlugin
- * @invite undefined
- * @authorLink undefined
- * @donate undefined
- * @patreon undefined
- * @website
- * @source
+ * @name CodingDND
+ * @invite https://joindtwm.net/join
+ * @authorID 395598378387636234
+ * @website https://github.com/SMC242/CodingDND
+ * @source https://github.com/SMC242/CodingDND/tree/master/src/dst/CodingDND.js
  */
 
 /**
 @cc_on
-@if (@_jscript)
+@if (@_jscript) 
 // Offer to self-install for clueless users that try to run this directly.
 var shell = WScript.CreateObject("WScript.Shell");
 var fs = new ActiveXObject("Scripting.FileSystemObject");
@@ -33,6 +31,9 @@ WScript.Quit();
 @else@
 */
 
+// @ts-ignore
+const Bapi = BdApi;
+
 module.exports = (() => {
   const config = {
     info: {
@@ -47,34 +48,19 @@ module.exports = (() => {
       version: "0.0.0",
       description: "Getting started",
       github: "https://github.com/SMC242/CodingDND",
-      github_raw: "P",
+      github_raw:
+        "https://github.com/SMC242/CodingDND/tree/master/src/dst/CodingDND.js",
     },
     changelog: [
       { title: "New Stuff", items: ["Added more settings", "Added changelog"] },
-      {
-        title: "Bugs Squashed",
-        type: "fixed",
-        items: ["React errors on reload"],
-      },
-      {
-        title: "Improvements",
-        type: "improved",
-        items: ["Improvements to the base plugin"],
-      },
-      {
-        title: "On-going",
-        type: "progress",
-        items: [
-          "More modals and popouts being added",
-          "More classes and modules being added",
-        ],
-      },
     ],
-    main: "index.js",
+    main: "CodingDND.js",
   };
 
+  // @ts-ignore
   return !global.ZeresPluginLibrary
     ? class {
+        _config: Object;
         constructor() {
           this._config = config;
         }
@@ -91,7 +77,7 @@ module.exports = (() => {
           return config.info.version;
         }
         load() {
-          BdApi.showConfirmationModal(
+          Bapi.showConfirmationModal(
             "Library Missing",
             `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`,
             {
@@ -108,7 +94,7 @@ module.exports = (() => {
                     await new Promise((r) =>
                       require("fs").writeFile(
                         require("path").join(
-                          BdApi.Plugins.folder,
+                          Bapi.Plugins.folder,
                           "0PluginLibrary.plugin.js"
                         ),
                         body,
@@ -128,22 +114,9 @@ module.exports = (() => {
         const plugin = (Plugin, Library) => {
           const { Logger, Patcher, Settings } = Library;
 
-          return class ExamplePlugin extends Plugin {
+          return class CodingDND extends Plugin {
             constructor() {
               super();
-              this.defaultSettings = {};
-              this.defaultSettings.color = "#ff0000";
-              this.defaultSettings.option = 50;
-              this.defaultSettings.keybind = [162, 74];
-              this.defaultSettings.radio = "weiner";
-              this.defaultSettings.slider1 = 30;
-              this.defaultSettings.slider2 = 54;
-              this.defaultSettings.textbox = "nothing";
-              this.defaultSettings.switch1 = false;
-              this.defaultSettings.switch2 = true;
-              this.defaultSettings.switch3 = true;
-              this.defaultSettings.switch4 = false;
-              this.defaultSettings.file = undefined;
             }
 
             onStart() {
@@ -162,156 +135,14 @@ module.exports = (() => {
               return Settings.SettingPanel.build(
                 this.saveSettings.bind(this),
                 new Settings.SettingGroup("Example Plugin Settings").append(
-                  new Settings.Textbox(
-                    "Textbox",
-                    "This should be a description of what this setting is about or blank",
-                    this.settings.textbox,
-                    (e) => {
-                      this.settings.textbox = e;
-                    }
-                  ),
-                  new Settings.Dropdown(
-                    "Select",
-                    "This should be a description of what this setting is about or blank",
-                    this.settings.option,
-                    [
-                      { label: "Test 1", value: "weiner" },
-                      { label: "Test 2", value: 50 },
-                      {
-                        label: "Test 3",
-                        value: JSON.stringify({
-                          label: "Test 1",
-                          value: "weiner",
-                        }),
-                      },
-                    ],
-                    (e) => {
-                      this.settings.option = e;
-                    }
-                  ),
-                  new Settings.SettingGroup("Example Plugin SubSettings", {
-                    shown: true,
-                  }).append(
-                    new Settings.RadioGroup(
-                      "Generic RadioGroup",
-                      "This should be a description of what this setting is about or blank",
-                      this.settings.radio,
-                      [
-                        {
-                          name: "Test 1",
-                          value: "weiner",
-                          desc: "This is the first test",
-                          color: "#ff0000",
-                        },
-                        {
-                          name: "Test 2",
-                          value: 50,
-                          desc: "This is the second test",
-                          color: "#00ff00",
-                        },
-                        {
-                          name: "Test 3",
-                          value: JSON.stringify({
-                            label: "Test 1",
-                            value: "weiner",
-                          }),
-                          desc: "This is the third test",
-                          color: "#0000ff",
-                        },
-                      ],
-                      (e) => {
-                        this.settings.radio = e;
-                      }
-                    ),
-                    new Settings.Switch(
-                      "Switch1",
-                      "This should be a description of what this setting is about or blank",
-                      this.settings.switch1,
-                      (e) => {
-                        this.settings.switch1 = e;
-                      }
-                    ),
-                    new Settings.Switch(
-                      "Switch2",
-                      "This should be a description of what this setting is about or blank",
-                      this.settings.switch2,
-                      (e) => {
-                        this.settings.switch2 = e;
-                      }
-                    ),
-                    new Settings.Switch(
-                      "Switch3",
-                      "This should be a description of what this setting is about or blank",
-                      this.settings.switch3,
-                      (e) => {
-                        this.settings.switch3 = e;
-                      }
-                    ),
-                    new Settings.Switch(
-                      "Switch4",
-                      "This should be a description of what this setting is about or blank",
-                      this.settings.switch4,
-                      (e) => {
-                        this.settings.switch4 = e;
-                      }
-                    )
-                  )
-                ),
-
-                new Settings.SettingGroup("Example Advanced Settings").append(
-                  new Settings.ColorPicker(
-                    "Color Example",
-                    "This should be a description of what this setting is about or blank",
-                    this.settings.color,
-                    (e) => {
-                      this.settings.color = e;
-                    }
-                  ),
-                  new Settings.Keybind(
-                    "Keybind",
-                    "This should be a description of what this setting is about or blank",
-                    this.settings.keybind,
-                    (e) => {
-                      this.settings.keybind = e;
-                    }
-                  ),
-                  new Settings.Slider(
-                    "Slider",
-                    "This should be a description of what this setting is about or blank",
-                    0,
-                    100,
-                    this.settings.slider1,
-                    (e) => {
-                      this.settings.slider1 = e;
-                    }
-                  ),
-                  new Settings.Slider(
-                    "Slider2",
-                    "This should be a description of what this setting is about or blank",
-                    0,
-                    90,
-                    this.settings.slider2,
-                    (e) => {
-                      this.settings.slider2 = e;
-                    },
-                    {
-                      markers: [0, 9, 18, 27, 36, 45, 54, 63, 72, 81, 90],
-                      stickToMarkers: true,
-                    }
-                  ),
-                  new Settings.FilePicker(
-                    "FilePicker",
-                    "This should be a description of what this setting is about or blank",
-                    (e) => {
-                      if (e) this.settings.file = e.path;
-                    }
-                  )
+                  null
                 )
               );
             }
           };
         };
         return plugin(Plugin, Api);
+        // @ts-ignore
       })(global.ZeresPluginLibrary.buildPlugin(config));
 })();
 /*@end@*/
